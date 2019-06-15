b4a0112f84b82bd17967791c9aae624e563a218a
Alex Wilson <alex+github@probablyfine.co.uk>
2019-06-15 21:16:55 +0100

    feat(initial-commit): Gitignore, working plugin scaffold

---
+++ src/main/groovy/uk/co/probablyfine/gradle/TestCommitRevertPlugin.groovy
@@ -0,0 +1,39 @@
+package uk.co.probablyfine.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
+
+class TestCommitRevertExtension {
+    String commitCommand = 'git commit --all --message TCR'
+    String revertCommand = 'git reset --hard HEAD'
+}
+
+class TestCommitRevertPlugin implements Plugin<Project> {
+
+    static void runCommand(String command) {
+        println command.execute().text
+    }
+
+    static Closure commitRevert(Project project) {
+        def extension = project.extensions.findByType(TestCommitRevertExtension)
+
+        return { description, result ->
+            // Only the final summary has a null parent
+            // This is an individual test run, so ignore
+            if (description.parent) return
+
+            runCommand(result.failedTestCount > 0 ? extension.revertCommand : extension.commitCommand)
+        }
+    }
+
+    @Override
+    void apply(Project project) {
+
+        project.extensions.create('testCommitRevert', TestCommitRevertExtension)
+
+        project.tasks.withType(Test) {
+            afterSuite commitRevert(project)
+        }
+    }
+}
